9177c46fafe549f7c83c12c12cf32ece86165088
Westa710 <pleiades1424@gmail.com>
2026-07-27 15:32:13 +0900

    feat: ただいまと言ってきますの最低限のルーティンを作成

--- src/application/routines/runArrivalRoutine.js
+++ src/application/routines/runArrivalRoutine.js
@@ -1,10 +1,22 @@
-const { isDevicePowerAboveThreshold } = require('../../switchbot/devices/isDevicePowerAboveThreshold')
+const { setDevicePower } = 
+    require('../../switchbot/devices/setDevicePower')
 
 
 /* 
 1．電気をONにする
+2. テレビのハブをONにする
 */
 async function runArrivalRoutine() {
+
+    await setDevicePower({ 
+        deviceId: process.env.INFRARED_CEILING_LIGHT_ID,
+        power: 'on'
+    });
+
+    await setDevicePower({ 
+        deviceId: process.env.HUB_ID,
+        power: 'on'
+    });
     
     return { executed: true }
 
--- src/application/routines/runDepartureRoutine.js
+++ src/application/routines/runDepartureRoutine.js
@@ -1,6 +1,9 @@
-
-const { isDevicePowerAboveThreshold } = require('../../switchbot/devices/isDevicePowerAboveThreshold')
-
+const { isDevicePowerAboveThreshold } =
+    require('../../switchbot/devices/isDevicePowerAboveThreshold')
+const { setDevicePower } = 
+    require('../../switchbot/devices/setDevicePower') 
+const { toggleInfraredDevicePower } = 
+    require('../../switchbot/devices/toggleInfraredDevicePower')
 
 /* 
 1．電気をOFFにする
@@ -8,6 +11,23 @@ const { isDevicePowerAboveThreshold } = require('../../switchbot/devices/isDevic
 　 テレビがOFF(powerが30未満)なら、ハブミニの電源をOFFにする
 */
 async function runDepartureRoutine() {
+
+    await setDevicePower({ 
+        deviceId: process.env.INFRARED_CEILING_LIGHT_ID,
+        power: 'off'
+    });
+    
+    if(await isDevicePowerAboveThreshold({ threshold: 30 })){
+        await toggleInfraredDevicePower()
+
+    }
+
+    await setDevicePower({
+        deviceID: process.env.HUB_ID,
+        power: 'off'
+    });
+    // TODO: 現状、テレビがONの時はTV_PLUGをトグルした直後にハブの電源をOFFにする。
+    //       これで動作しない場合は、sleepさせるなどで時間を空ける必要があるかも
     
     return { executed: true }
 
